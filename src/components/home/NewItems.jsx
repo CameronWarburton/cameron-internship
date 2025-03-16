3eede679f0a9407771d386cfd456b9aecd9fb288
Cameron <cameron.cw@outlook.com>
2025-03-16 22:02:24 +0000

    Dynamic Explore Items Page

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
+import CountdownTimer from "../CountdownTimer";
+import NFTItem from "../NFTItem";
 
 const NewItems = () => {
   const [items, setItems] = useState([]);
@@ -21,49 +23,6 @@ const NewItems = () => {
   setTimeout(() => {
     setLoading(false);
   }, 3000);
-
-  function CountdownTimer({ expiryDate }) {
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(expiryDate));
-
-    useEffect(() => {
-      if (!expiryDate) return;
-
-      const interval = setInterval(() => {
-        const updatedTime = calculateTimeLeft(expiryDate);
-        setTimeLeft(updatedTime);
-
-        if (updatedTime.total <= 0) {
-          clearInterval(interval);
-        }
-      }, 1000);
-
-      return () => clearInterval(interval);
-    }, [expiryDate]);
-
-    function calculateTimeLeft(expiry) {
-      const now = Date.now();
-      const difference = expiry - now;
-
-      if (difference <= 0) {
-        return { hours: 0, minutes: 0, seconds: 0, total: 0 };
-      }
-
-      return {
-        hours: Math.floor(difference / (1000 * 60 * 60)),
-        minutes: Math.floor((difference / (1000 * 60)) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-        total: difference,
-      };
-    }
-
-    if (timeLeft.total < +0) return null;
-
-    return (
-      <div className="de_countdown">
-        {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s
-      </div>
-    );
-  }
 
   function Arrow(props) {
     const { className, style, onClick } = props;
@@ -175,66 +134,7 @@ const NewItems = () => {
                   </div>
                 ))
               : items.map((item, index) => (
-                  <div key={index} className="px-1">
-                    <div className="nft__item">
-                      <div className="author_list_pp">
-                        <Link
-                          to={`/item-details/${item.authorId}`}
-                          data-bs-toggle="tooltip"
-                          data-bs-placement="top"
-                          title="Creator: Monica Lucas"
-                        >
-                          <img
-                            className="lazy"
-                            src={`${item.authorImage}`}
-                            alt=""
-                          />
-                          <i className="fa fa-check"></i>
-                        </Link>
-                      </div>
-                      {item.expiryDate && <CountdownTimer expiryDate={item.expiryDate} />}
-
-                      <div className="nft__item_wrap">
-                        <div className="nft__item_extra">
-                          <div className="nft__item_buttons">
-                            <button>Buy Now</button>
-                            <div className="nft__item_share">
-                              <h4>Share</h4>
-                              <a href="" target="_blank" rel="noreferrer">
-                                <i className="fa fa-facebook fa-lg"></i>
-                              </a>
-                              <a href="" target="_blank" rel="noreferrer">
-                                <i className="fa fa-twitter fa-lg"></i>
-                              </a>
-                              <a href="">
-                                <i className="fa fa-envelope fa-lg"></i>
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-
-                        <Link to={`/item-details/${item.nftId}`}>
-                          <img
-                            src={`${item.nftImage}`}
-                            className="lazy nft__item_preview"
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div className="nft__item_info">
-                        <Link to="/item-details">
-                          <h4>{`${item.title}`}</h4>
-                        </Link>
-                        <div className="nft__item_price">
-                          {`${item.price}`} ETH
-                        </div>
-                        <div className="nft__item_like">
-                          <i className="fa fa-heart"></i>
-                          <span>{`${item.likes}`}</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <NFTItem key={index} item={item} />
                 ))}
           </Slider>
         </div>
