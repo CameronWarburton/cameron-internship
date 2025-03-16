2d1109945d1865c631e43724a8af9b54098cfb79
Cameron <cameron.cw@outlook.com>
2025-03-16 16:23:42 +0000

    Dynamic Top Sellers Feature

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+    async function getData() {
+      const response = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+      );
+      const results = response.data;
+      setItems(results);
+    }
+    useEffect(() => {
+      getData();
+    }, []);
+  
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +35,44 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+            {loading 
+            ? new Array(12).fill(0).map((_, index) => (
+                  <li className="px-1" key={index}>
+                    <div className="author_list_pp">
+                      <div
+                        className="skeleton-box"
+                        style={{ width: "50px", height: "50px", borderRadius: "50%",}}
+                      ></div>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="author_list_info">
+                      <div
+                        className="skeleton-box"
+                        style={{ width: "100px", height: "18px" }}
+                      ></div>
+                       <br />
+                      <div
+                        className="skeleton-box"
+                        style={{ width: "30px", height: "15px" }}
+                      ></div>
+                    </div>
+                  </li>
+                ))
+              :items.map((items, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/${items.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={`${items.authorImage}`}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to={`/${items.authorId}`}>{`${items.authorName}`}</Link>
+                    <span>{`${items.price}`} ETH</span>
                   </div>
                 </li>
               ))}
